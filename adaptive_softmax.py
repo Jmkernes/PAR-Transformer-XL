03efaab0faf3c8be3030b1421f9ae2539e11926f
Jmkernes <Jmkernes@ucla.edu>
2021-01-22 19:37:31 -0800

    Fixed a bug where tf.boolean_mask() reduced dimension of input to tail_W dense layer, thereby giving different input between loss and logit calculations

--- adaptive_softmax.py
+++ adaptive_softmax.py
@@ -29,19 +29,22 @@ class AdaptiveSoftmax(tf.keras.layers.Layer):
         self.head_w = None
         self.tail_w = None
 
+    def get_config(self):
+        base_config = super().get_config()
+        return {**base_config, "proj_factor": 4, "proj_dims":[]}
+
     def build(self, input_shape):
-        hidden_dim = tf.cast(input_shape[-1], tf.int32) # better be <2^32
-        self.proj_factor = tf.cast(self.proj_factor, tf.int32) # same^^
+        hidden_dim = input_shape[-1]
         if self.proj_dims:
             assert len(self.proj_dims)==self.cluster_num
         else:
-            self.proj_dims = hidden_dim/self.proj_factor**tf.range(1, self.cluster_num+1)
-            self.proj_dims = tf.maximum(self.proj_dims, 1)
+            self.proj_dims = [tf.math.maximum(hidden_dim/self.proj_factor**i, 1)
+            for i in range(1, self.cluster_num+1)]
 
         # initialize dense layers for head. reserve cluster_num spots for
         # each cluster probability
         head_dim = self.cutoffs[0]+self.cluster_num
-        self.head_w = tf.keras.layers.Dense(head_dim, name='head_w')
+        self.head_w = tf.keras.layers.Dense(head_dim, name='ada_softmax_head_w',input_shape=[None, hidden_dim])
 
         # initialize dense layers for each cluster.
         self.tail_w = []
@@ -49,15 +52,15 @@ class AdaptiveSoftmax(tf.keras.layers.Layer):
             tail_dim = self.cutoffs[i+1]-self.cutoffs[i]
             self.tail_w.append(
                 tf.keras.models.Sequential([
-                    tf.keras.layers.Dense(self.proj_dims[i],
-                    name=f"tail{i}_proj", input_shape=[hidden_dim]),
-                    tf.keras.layers.Dense(tail_dim, name=f"tail{i}_w")
-                ]))
+                    tf.keras.layers.Dense(self.proj_dims[i], name=f"ada_softmax_tail{i+1}_proj", input_shape=[None, hidden_dim]),
+                    tf.keras.layers.Dense(tail_dim, name=f"ada_softmax_tail{i+1}_w")
+                ])
+            )
         super().build(input_shape)
 
     def _loss(self, inp, labels, reduction='auto'):
         head_labels = labels
-        loss = tf.sparse.SparseTensor([[0,0]],[0.], tf.cast(tf.shape(labels), tf.int64)) # this might break in graph mode. check.
+        loss = tf.sparse.SparseTensor([[0,0]],[0.], tf.cast(tf.shape(labels), tf.int64))
         for i in range(self.cluster_num):
             mask = tf.logical_and(
                 tf.greater_equal(labels, self.cutoffs[i]),
@@ -72,15 +75,20 @@ class AdaptiveSoftmax(tf.keras.layers.Layer):
             cluster_label = tf.cast(self.cutoffs[0]+i, head_labels.dtype)
             head_labels = tf.where(mask, cluster_label, head_labels)
 
+            # NOTE: taking boolean mask will help with efficiency, which is the whole reason
+            # that we need a built-in loss (to only calculate softmax for the bin that the label tells us to)
+            # BUT it will collapse a dimension, i.e. it maps (bsz, seq_len, d_model) to
+            # (num_true, d_model) this is a different input shape to the dense layer than when we call the
+            # logits function. Thus, we must pad the zero dimension to be consistent.
             tail_inp = tf.boolean_mask(inp, mask) # (num_in_clust, hidden_dim)
-            tail_logits = self.tail_w[i](tail_inp)
+            tail_logits = self.tail_w[i](tf.expand_dims(tail_inp,0))[0] # expand then contract back
             tail_labels = tf.boolean_mask(labels-self.cutoffs[i], mask)
             tail_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
                 tail_labels, tail_logits)
             aligned_tail_loss = tf.sparse.SparseTensor(
                 indices=tf.where(mask), values=tail_loss,
                 dense_shape=tf.cast(tf.shape(labels), tf.int64)
-            ) # Same, might also break in graph mode b/c of labels.shpe
+            )
 
             # don't convert these to dense yet. slightly more efficient.
             loss = tf.sparse.add(loss, aligned_tail_loss)
